5d4e3e50c1a4f07bfdf1432f799979e1fc094cb8
yahiro <yahiro07@users.noreply.github.com>
2026-05-28 10:24:38 +0900

    cleanup code

--- spacbar/src/index.tsx
+++ spacbar/src/index.tsx
@@ -84,70 +84,24 @@ function setupUnitInstance() {
 }
 setupUnitInstance();
 
-function frequencyToMidiNoteNumber(freq: number) {
-  return 69 + 12 * Math.log2(freq / 440);
-}
-
-function degToRad(deg: number) {
-  return deg * (Math.PI / 180);
-}
-
 function renderCanvasSpectrum(
   canvas: HTMLCanvasElement,
   fftData: Float32Array,
-  sampleRate: number,
 ) {
   const ctx = canvas.getContext("2d")!;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "#000";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  // ctx.lineWidth = 4;
-  // ctx.strokeStyle = "#0f03";
-  // ctx.moveTo(0, canvas.height / 2);
-
-  const topFreq = sampleRate / 2;
   for (let i = 0; i < fftData.length; i++) {
     const pp = i / (fftData.length - 1);
-
-    let value = mapUnaryFrom(fftData[i], -120, 0, true);
-    if (0) {
-      value *= (1 - pp) * (1 - pp);
-      const frequency = pp * topFreq;
-      const noteNumber = frequencyToMidiNoteNumber(frequency);
-      const angle = degToRad(noteNumber * 30);
-
-      const cx = canvas.width / 2;
-      const cy = canvas.height / 2;
-      const baseRadius = cy;
-
-      // const octave = (noteNumber / 12) >>> 0;
-      // const ra = linearInterpolate(octave, 0, 10, 0.2, 0.8, true);
-      const ra = 0.5 - value * 0.5;
-      const rb = 0.5 + value * 0.5;
-
-      const pax = cx + Math.cos(angle) * ra * baseRadius;
-      const pay = cy + Math.sin(angle) * ra * baseRadius;
-      const pbx = cx + Math.cos(angle) * rb * baseRadius;
-      const pby = cy + Math.sin(angle) * rb * baseRadius;
-
-      // const hue = pp * 360;
-      // const color = `hsla(${hue}, ${100}%, ${value * 100}%, .3)`;
-      const color = "#08f4";
-
-      ctx.strokeStyle = color;
-      ctx.beginPath();
-      ctx.moveTo(pax, pay);
-      ctx.lineTo(pbx, pby);
-      ctx.stroke();
-    } else {
-      const px = pp * canvas.width;
-      const py = mapUnaryTo(value, canvas.height, 0);
-      ctx.strokeStyle = "#0f0";
-      ctx.beginPath();
-      ctx.moveTo(px, canvas.height);
-      ctx.lineTo(px, py);
-      ctx.stroke();
-    }
+    const value = mapUnaryFrom(fftData[i], -120, 0, true);
+    const px = pp * canvas.width;
+    const py = mapUnaryTo(value, canvas.height, 0);
+    ctx.strokeStyle = "#0f0";
+    ctx.beginPath();
+    ctx.moveTo(px, canvas.height);
+    ctx.lineTo(px, py);
+    ctx.stroke();
   }
 }
 
@@ -160,7 +114,7 @@ const SpectrumView0 = () => {
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!(fftData && canvas && sampleRate)) return;
-    renderCanvasSpectrum(canvas, fftData, sampleRate);
+    renderCanvasSpectrum(canvas, fftData);
   }, [fftData, sampleRate]);
 
   return (
