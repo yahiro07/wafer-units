be86864d5b6dcb1294f109611e652dc0894c702d
yahiro <yahiro07@users.noreply.github.com>
2026-06-11 17:24:22 +0900

    make custom element unit

---
+++ packages/bseq1/src/app.tsx
@@ -0,0 +1,94 @@
+import { Button } from "@/components/button";
+import { Icons } from "@/components/icons";
+import { Knob } from "@/components/knob";
+import { FeNumberSliderBox } from "@/components/number-slider-box";
+import { NoteClockView } from "@/organisms/note-clock-view";
+import { StepButton } from "@/organisms/step-button";
+import { StepRollBar } from "@/organisms/step-roll-bar";
+import { drivers } from "@/sequencer/drivers";
+import { appReaders, appState, uiActions } from "@/store/store";
+import { getNoteName } from "@/utils/note-name-helper";
+
+const TopBar = () => {
+  return (
+    <div class="w-full flex-c gap-3 bg-zinc-600 px-2 py-3">
+      <Button
+        active={appState.playing}
+        onClick={() => uiActions.setPlaying(!appState.playing)}
+      >
+        <Icons.Play />
+      </Button>
+      {/* <FeNumberSliderBox
+        value={appState.bpm}
+        onChange={uiActions.setBpm}
+        min={90}
+        max={140}
+        step={1}
+        fracDigits={0}
+      /> */}
+      <div class="flex-ha gap-1">
+        <div class="text-white">note</div>
+        <FeNumberSliderBox
+          value={appState.noteNumber}
+          onChange={uiActions.setNoteNumber}
+          min={12}
+          max={48}
+          step={1}
+          fracDigits={0}
+        />
+        <NoteClockView noteNumber={appState.noteNumber} />
+        <div class="w-[16px] text-white">
+          {getNoteName(appState.noteNumber)}
+        </div>
+      </div>
+      <div class="flex-ha gap-1">
+        <div class="text-white">duty</div>
+        <Knob
+          value={appState.duty}
+          onChange={uiActions.setDuty}
+          min={0.1}
+          max={1}
+          step={0.01}
+        />
+      </div>
+    </div>
+  );
+};
+
+const PageRoot = () => {
+  return (
+    <div class="flex-c text-[#444]">
+      <div class="w-[340px] h-[210px] flex-vc bg-zinc-100 border border-[#445]">
+        <TopBar />
+        <div class="grow flex-vc gap-3">
+          <StepRollBar
+            stepCodes={appState.stepCodes}
+            currentStepIndex={appState.currentStepIndex}
+            playing={appReaders.somePlaying()}
+            duty={appState.duty}
+          />
+          <div class="flex-ha gap-3">
+            {appState.stepCodes.map((code, index) => (
+              <StepButton
+                index={index}
+                code={code}
+                active={
+                  appState.currentStepIndex === index &&
+                  appReaders.somePlaying()
+                }
+                onChange={(nextCode) => uiActions.setStepCode(index, nextCode)}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const App = () => {
+  console.log("bseq1 0914");
+  drivers.setupAll();
+
+  return <PageRoot />;
+};
--- packages/bseq1/src/index.tsx
+++ packages/bseq1/src/index.tsx
@@ -1,99 +1,40 @@
-import "./page.css";
-import "mofus/ax-ui/utility-classes.css";
-import { mountAppRoot } from "mofus/ax-solid";
-import { Button } from "@/components/button";
-import { Icons } from "@/components/icons";
-import { Knob } from "@/components/knob";
-import { FeNumberSliderBox } from "@/components/number-slider-box";
-import { NoteClockView } from "@/organisms/note-clock-view";
-import { StepButton } from "@/organisms/step-button";
-import { StepRollBar } from "@/organisms/step-roll-bar";
-import { drivers } from "@/sequencer/drivers";
-import { appReaders, appState, uiActions } from "@/store/store";
-import { getNoteName } from "@/utils/note-name-helper";
+import cssText2 from "mofus/ax-ui/utility-classes.css?inline";
+import { render } from "solid-js/web";
+import { App } from "./app";
+import cssText from "./page.css?inline";
 
-const TopBar = () => {
-  return (
-    <div class="w-full flex-c gap-3 bg-zinc-600 px-2 py-3">
-      <Button
-        active={appState.playing}
-        onClick={() => uiActions.setPlaying(!appState.playing)}
-      >
-        <Icons.Play />
-      </Button>
-      {/* <FeNumberSliderBox
-        value={appState.bpm}
-        onChange={uiActions.setBpm}
-        min={90}
-        max={140}
-        step={1}
-        fracDigits={0}
-      /> */}
-      <div class="flex-ha gap-1">
-        <div class="text-white">note</div>
-        <FeNumberSliderBox
-          value={appState.noteNumber}
-          onChange={uiActions.setNoteNumber}
-          min={12}
-          max={48}
-          step={1}
-          fracDigits={0}
-        />
-        <NoteClockView noteNumber={appState.noteNumber} />
-        <div class="w-[16px] text-white">
-          {getNoteName(appState.noteNumber)}
-        </div>
-      </div>
-      <div class="flex-ha gap-1">
-        <div class="text-white">duty</div>
-        <Knob
-          value={appState.duty}
-          onChange={uiActions.setDuty}
-          min={0.1}
-          max={1}
-          step={0.01}
-        />
-      </div>
-    </div>
-  );
-};
+export default class UnitElement extends HTMLElement {
+  isMounted: boolean;
+  disposeRender: (() => void) | null = null;
 
-const PageRoot = () => {
-  return (
-    <div class="w-dvw h-dvh flex-c text-[#444]">
-      <div class="w-[340px] h-[210px] flex-vc bg-zinc-100 border border-[#445]">
-        <TopBar />
-        <div class="grow flex-vc gap-3">
-          <StepRollBar
-            stepCodes={appState.stepCodes}
-            currentStepIndex={appState.currentStepIndex}
-            playing={appReaders.somePlaying()}
-            duty={appState.duty}
-          />
-          <div class="flex-ha gap-3">
-            {appState.stepCodes.map((code, index) => (
-              <StepButton
-                index={index}
-                code={code}
-                active={
-                  appState.currentStepIndex === index &&
-                  appReaders.somePlaying()
-                }
-                onChange={(nextCode) => uiActions.setStepCode(index, nextCode)}
-              />
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.isMounted = false;
+  }
 
-const App = () => {
-  console.log("bseq1 0914");
-  drivers.setupAll();
+  mountApp() {}
 
-  return <PageRoot />;
-};
+  connectedCallback() {
+    if (this.isMounted || !this.shadowRoot) return;
 
-mountAppRoot(() => <App />);
+    const style = document.createElement("style");
+    style.dataset.unit1Styles = "true";
+    style.textContent = cssText + cssText2;
+    this.shadowRoot.appendChild(style);
+
+    this.disposeRender = render(() => <App />, this.shadowRoot);
+    this.isMounted = true;
+  }
+
+  disconnectedCallback() {
+    if (this.isMounted && this.shadowRoot) {
+      setTimeout(() => {
+        if (!this.shadowRoot) return;
+        this.disposeRender?.();
+        this.disposeRender = null;
+        this.isMounted = false;
+      }, 0);
+    }
+  }
+}
