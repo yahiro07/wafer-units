dc39f4ee87cd633a8d5fe0cb70bb0d0a3f00fe93
yahiro <yahiro07@users.noreply.github.com>
2026-06-12 22:57:41 +0900

    fix font

--- packages/mini-synth-ge/src/index.tsx
+++ packages/mini-synth-ge/src/index.tsx
@@ -4,7 +4,14 @@ import { MainApp } from "@/MainApp";
 import cssText from "./styles/page.css?inline";
 import cssText2 from "./styles/utility-classes.css?inline";
 
+const webFontUrls = [
+  "https://fonts.googleapis.com/css2?family=Orbitron:wght@400..700&display=swap",
+];
+
 export default createCustomElementClass(
   (root) => render(() => <MainApp />, root),
-  [cssText, cssText2],
+  {
+    cssTexts: [cssText, cssText2],
+    stylesheetUrls: webFontUrls,
+  },
 );
