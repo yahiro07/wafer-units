7c70ea0d761e2e863032448ae08af39cdc5c8bae
yahiro <yahiro07@users.noreply.github.com>
2026-06-12 21:44:57 +0900

    add project

---
+++ packages/rtfr/vite.config.ts
@@ -0,0 +1,26 @@
+import preact from "@preact/preset-vite";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "./",
+  define: { "process.env.NODE_ENV": JSON.stringify("production") },
+  plugins: [preact(), tailwindcss()],
+  resolve: {
+    alias: {
+      react: "preact/compat",
+      "react-dom": "preact/compat",
+      "react/jsx-runtime": "preact/jsx-runtime",
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.tsx"),
+      formats: ["es"],
+      fileName: "index",
+    },
+    outDir: "../../dist/rtfr",
+    emptyOutDir: true,
+  },
+});
