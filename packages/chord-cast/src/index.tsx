a37f114d9b55cad6ac6e52915d84b7815ea7ba5d
yahiro <yahiro07@users.noreply.github.com>
2026-06-12 21:19:00 +0900

    implement unit

---
+++ packages/chord-cast/src/index.tsx
@@ -0,0 +1,27 @@
+import cssText2 from "mofur/ax-ui/utility-classes.css?inline";
+import cssText3 from "mofur-components/style.css?inline";
+import { render } from "preact";
+import { queryUnitInterfaceForModule } from "wus-unit-types";
+import { createCustomElementClass } from "wus-unit-types/unit-helper";
+import cssText from "./page.css?inline";
+import { createChordProgressionUnit } from "./unit";
+
+function createEntryClass() {
+  const unitInterface = queryUnitInterfaceForModule("wus-v01", import.meta.url);
+  if (!unitInterface) {
+    throw new Error("undefined unit interface");
+  }
+  const unit = createChordProgressionUnit(unitInterface);
+
+  return createCustomElementClass(
+    (shadowRoot) => {
+      render(<unit.RenderUi />, shadowRoot);
+      return () => {
+        render(null, shadowRoot);
+      };
+    },
+    [cssText, cssText2, cssText3],
+  );
+}
+
+export default createEntryClass();
