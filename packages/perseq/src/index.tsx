397be3437729ef75b1c667c29df13398c1871df3
yahiro <yahiro07@users.noreply.github.com>
2026-06-13 18:17:38 +0900

    add project
    # Conflicts:
    #	cspell.json
    #	pnpm-lock.yaml

---
+++ packages/perseq/src/index.tsx
@@ -0,0 +1,19 @@
+import cssText2 from "mofur/ax-ui/utility-classes.css?inline";
+import cssText3 from "mofur-components/style.css?inline";
+import { render } from "preact";
+import { createCustomElementClass } from "wus-unit-types/unit-helper";
+import { App } from "@/unit";
+import cssText from "./page.css?inline";
+
+const webFontUrl =
+  "https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap";
+
+export default createCustomElementClass(
+  (shadowRoot) => {
+    render(<App />, shadowRoot);
+    return () => {
+      render(null, shadowRoot);
+    };
+  },
+  { cssTexts: [cssText, cssText2, cssText3], stylesheetUrls: [webFontUrl] },
+);
