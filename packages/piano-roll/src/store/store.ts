5f0b1baa665c9f0c908b1e3d3e6baa7e10515cfd
yahiro <yahiro07@users.noreply.github.com>
2026-06-14 19:36:58 +0900

    change default data

--- packages/piano-roll/src/store/store.ts
+++ packages/piano-roll/src/store/store.ts
@@ -3,10 +3,12 @@ import { Note } from "@/store/types";
 
 const defaultNotes: Note[] = 0
   ? [
-      { relativeNoteNumber: 14, stepPosition: 0, stepDuration: 2 },
-      { relativeNoteNumber: 15, stepPosition: 2, stepDuration: 2 },
-      { relativeNoteNumber: 16, stepPosition: 4, stepDuration: 2 },
-      { relativeNoteNumber: 17, stepPosition: 6, stepDuration: 2 },
+      { stepPosition: 0, stepDuration: 2, relativeNoteNumber: 14 },
+      { stepPosition: 2, stepDuration: 2, relativeNoteNumber: 15 },
+      { stepPosition: 4, stepDuration: 2, relativeNoteNumber: 16 },
+      { stepPosition: 6, stepDuration: 2, relativeNoteNumber: 17 },
+      { stepPosition: 16, stepDuration: 8, relativeNoteNumber: 17 },
+      { stepPosition: 32, stepDuration: 16, relativeNoteNumber: 14 },
     ]
   : [];
 
