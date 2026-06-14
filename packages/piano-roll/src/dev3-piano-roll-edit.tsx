a69af58297f81757e8b547bcc77f39e41876d612
yahiro <yahiro07@users.noreply.github.com>
2026-06-14 19:05:14 +0900

    make styling emotion based

--- packages/piano-roll/src/dev3-piano-roll-edit.tsx
+++ packages/piano-roll/src/dev3-piano-roll-edit.tsx
@@ -1,7 +1,61 @@
 import { css } from "@emotion/react";
 import { seqNumbers } from "mofur/ax";
 import { npx } from "mofur/ax-ui";
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
+
+function makeCssPianoRollBackgroundOctaveBlock(cellW: number, cellH: number) {
+  return css`
+    & > .grid-row {
+      display: flex;
+      background: #fff;
+    }
+
+    & > .grid-row > .grid-cell {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: ${npx(cellW)};
+      height: ${npx(cellH)};
+      border: 0.5px solid #8881;
+      color: #8884;
+      font-size: ${npx(12)};
+    }
+
+    & > .overlay-v,
+    & > .overlay-h {
+      position: absolute;
+      inset: 0;
+      pointer-events: none;
+    }
+
+    & > .overlay-v {
+      display: flex;
+      flex-direction: column;
+    }
+
+    & > .overlay-v > .overlay-v-top {
+      height: 57.14%;
+      border-bottom: 1px solid #ccc;
+    }
+
+    & > .overlay-h {
+      display: flex;
+    }
+
+    & > .overlay-h > .overlay-h-bar {
+      display: flex;
+      flex-grow: 1;
+      border: solid 0.5px #ccc;
+      border-right: none;
+      border-bottom: none;
+    }
+
+    & > .overlay-h > .overlay-h-bar > .overlay-h-split {
+      flex-grow: 1;
+      border: solid 0.5px #ccc8;
+    }
+  `;
+}
 
 const PianoRollBackgroundOctaveBlock = ({
   cellW,
@@ -12,23 +66,18 @@ const PianoRollBackgroundOctaveBlock = ({
   cellH: number;
   nx: number;
 }) => {
+  const rootCss = useMemo(
+    () => makeCssPianoRollBackgroundOctaveBlock(cellW, cellH),
+    [cellW, cellH],
+  );
   return (
-    <div className="relative">
+    <div className="relative" css={rootCss}>
       {seqNumbers(7).map((yi) => {
         return (
-          <div className="flex-h bg-[#fff]">
+          <div className="grid-row">
             {seqNumbers(nx).map((xi) => {
               return (
-                <div
-                  className="flex-c"
-                  style={{
-                    width: npx(cellW),
-                    height: npx(cellH),
-                    border: "0.5px solid #8881",
-                    color: "#8884",
-                    fontSize: npx(12),
-                  }}
-                >
+                <div className="grid-cell">
                   {xi % 2 === 0 && (yi === 3 || yi === 6) && "・"}
                 </div>
               );
@@ -37,21 +86,13 @@ const PianoRollBackgroundOctaveBlock = ({
         );
       })}
 
-      <div className="absolute-full flex-v">
-        <div className="h-[57.14%] border-b border-[#ccc]" />
+      <div className="overlay-v">
+        <div className="overlay-v-top" />
       </div>
-      <div className="absolute-full flex-h">
-        <div
-          className="flex-h"
-          style={{
-            flexGrow: 1,
-            border: "solid 0.5px #ccc",
-            borderRight: "none",
-            borderBottom: "none",
-          }}
-        >
+      <div className="overlay-h">
+        <div className="overlay-h-bar">
           {seqNumbers(2).map(() => (
-            <div style={{ flexGrow: 1, border: "solid 0.5px #ccc8" }} />
+            <div className="overlay-h-split" />
           ))}
         </div>
       </div>
