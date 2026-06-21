b1ce4ca49e5188bd928d003aab9cc53236b41ad9
yahiro <yahiro07@users.noreply.github.com>
2026-06-22 00:42:53 +0900

    [rtfr] use shared unit class

--- packages/rtfr/src/index.tsx
+++ packages/rtfr/src/index.tsx
@@ -1,33 +1,25 @@
 import cssText2 from "mofur/ax-ui/utility-classes.css?inline";
 import cssText3 from "mofur-components/style.css?inline";
 import { render } from "preact";
-import { createCustomElementClass } from "wafer-host/unit-helper";
-import { queryUnitInterfaceForModule } from "wafer-host/unit-types";
+import { createCustomElementSharableClass } from "wafer-host/unit-helper";
 import cssText from "./page.css?inline";
 import { createRtfrUnit } from "./unit";
 
 const webFontUrl =
   "https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap";
 
-function createEntryClass() {
-  const unitInterface = queryUnitInterfaceForModule(
-    "wafer-v01",
-    import.meta.url,
-  );
-  if (!unitInterface) {
-    throw new Error("undefined unit interface");
-  }
-  const unit = createRtfrUnit(unitInterface);
-
-  return createCustomElementClass(
-    (shadowRoot) => {
-      render(<unit.RenderUi />, shadowRoot);
-      return () => {
-        render(null, shadowRoot);
-      };
-    },
-    { cssTexts: [cssText, cssText2, cssText3], stylesheetUrls: [webFontUrl] },
-  );
-}
-
-export default createEntryClass();
+export default createCustomElementSharableClass(
+  (unitInterfaceProvider, shadowRoot) => {
+    const unitInterface =
+      unitInterfaceProvider.queryUnitInterface?.("wafer-v01");
+    if (!unitInterface) {
+      throw new Error("undefined unit interface");
+    }
+    const unit = createRtfrUnit(unitInterface);
+    render(<unit.RenderUi />, shadowRoot);
+    return () => {
+      render(null, shadowRoot);
+    };
+  },
+  { cssTexts: [cssText, cssText2, cssText3], stylesheetUrls: [webFontUrl] },
+);
