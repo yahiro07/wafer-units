3d31f0931ca63c71660f2a416fc38a7c169a0fc6
yahiro <yahiro07@users.noreply.github.com>
2026-06-25 04:02:26 +0900

    show debug piece file names

--- packages/drum-seq/src/ui/debug-ui.tsx
+++ packages/drum-seq/src/ui/debug-ui.tsx
@@ -1,9 +1,10 @@
 import clsx from "clsx";
 import { useEffect, useState } from "react";
 import { debugGlobal } from "@/audio/tone-player";
+import { pieceSampleUrls } from "@/base/constants";
 import { useAppContext } from "@/store/app-context";
 
-export const DebugUi = () => {
+const PlayButton = () => {
   const { actions } = useAppContext();
   const [playing, setPlaying] = useState(false);
   useEffect(() => {
@@ -22,16 +23,41 @@ export const DebugUi = () => {
     }
   }, [playing]);
   return (
+    <button
+      className={clsx(
+        "p-2 cursor-pointer",
+        playing ? "bg-green-500" : "bg-gray-400",
+      )}
+      onClick={() => setPlaying(!playing)}
+    >
+      play
+    </button>
+  );
+};
+
+const SampleNamesDisplay = () => {
+  const { store } = useAppContext();
+  const { pieces } = store.useSnapshot();
+  return (
     <div>
-      <button
-        className={clsx(
-          "p-2 cursor-pointer",
-          playing ? "bg-green-500" : "bg-gray-400",
-        )}
-        onClick={() => setPlaying(!playing)}
-      >
-        play
-      </button>
+      {pieces.map((piece) => {
+        return (
+          <div>
+            {piece.id}: {pieceSampleUrls[piece.id][piece.variationIndex]}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export const DebugUi = () => {
+  return (
+    <div className="w-full flex-c">
+      <div className="flex-ha gap-8">
+        <PlayButton />
+        <SampleNamesDisplay />
+      </div>
     </div>
   );
 };
