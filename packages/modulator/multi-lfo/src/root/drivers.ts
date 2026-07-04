c0b557f814a324ca45715c718cf353f1bf7c03bb
yahiro <yahiro07@users.noreply.github.com>
2026-07-04 21:01:10 +0900

    [multi-lfo] cleanup store subscription

--- packages/modulator/multi-lfo/src/root/drivers.ts
+++ packages/modulator/multi-lfo/src/root/drivers.ts
@@ -2,7 +2,7 @@ import { queryUnitInterface } from "wafer-host/unit-types";
 import { createSequencer } from "@/root/sequencer";
 import { store } from "@/root/store";
 
-const unitInterface = queryUnitInterface("wafer-v01");
+export const unitInterface = queryUnitInterface("wafer-v01");
 
 const sequencer = createSequencer(unitInterface);
 sequencer.setLfoSlots(store.state.slots);
--- packages/modulator/multi-lfo/src/web-entry/index.tsx
+++ packages/modulator/multi-lfo/src/web-entry/index.tsx
@@ -2,10 +2,17 @@ import "mofur/ax-ui/utility-classes.css";
 import { render } from "preact";
 import "./page.css";
 import { App } from "@/root/app";
+import { unitInterface } from "@/root/drivers";
+
+const rootElement = document.getElementById("app")!;
 
 render(
   <div className="w-dvw h-dvh bg-white flex-vc">
     <App />
   </div>,
-  document.getElementById("app")!,
+  rootElement,
 );
+
+unitInterface?.onIframeUnloading(() => {
+  render(null, rootElement);
+});
