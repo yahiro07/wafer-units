045390ed6b2aa3339c7c670d819acd8bfcf818ea
yahiro <yahiro07@users.noreply.github.com>
2026-07-10 20:39:15 +0900

    apply parameters while voicing

--- packages/synthesizers/s2/src/root/synthesizer.ts
+++ packages/synthesizers/s2/src/root/synthesizer.ts
@@ -46,6 +46,53 @@ function createVoice(bus: SynthesisBus) {
   const ampGain = audioContext.createGain();
   ampGain.gain.value = 0;
 
+  const state = {
+    noteNumber: -1,
+  };
+
+  const internal = {
+    applyParameters() {
+      if (state.noteNumber === -1) return;
+      const pr = bus.parameters;
+      const osc1Freq = calcOscFreq(
+        state.noteNumber,
+        pr.octave,
+        pr.osc1Coarse,
+        pr.osc1Fine,
+      );
+      const osc2Freq = calcOscFreq(
+        state.noteNumber,
+        pr.octave,
+        pr.osc2Coarse,
+        pr.osc2Fine,
+      );
+      osc1.type = "sawtooth";
+      osc2.type = "sawtooth";
+      // const tt = audioContext.currentTime + 0.01;
+      osc1.frequency.value = osc1Freq;
+      osc2.frequency.value = osc2Freq;
+      osc1Gain.gain.value = pr.oscMix;
+      osc2Gain.gain.value = 1 - pr.oscMix;
+    },
+    triggerAttack(time: number) {
+      ampGain.gain.cancelScheduledValues(time);
+      ampGain.gain.setValueAtTime(1, time);
+
+      const pr = bus.parameters;
+      const isSustain = pr.ampDecay === 1;
+      if (!isSustain) {
+        const decayTime = 0.01 + power2(pr.ampDecay) * 4;
+        ampGain.gain.exponentialRampToValueAtTime(1e-3, time + decayTime);
+      }
+    },
+    triggerRelease(time: number) {
+      const pr = bus.parameters;
+      const releaseTime = 0.01 + power2(pr.ampRelease) * 4;
+      // ampGain.gain.cancelScheduledValues(time);
+      ampGain.gain.exponentialRampToValueAtTime(1e-3, time + releaseTime);
+    },
+  };
+
   return {
     connects() {
       osc1.connect(osc1Gain);
@@ -65,41 +112,17 @@ function createVoice(bus: SynthesisBus) {
       osc2Gain.disconnect();
       ampGain.disconnect();
     },
+    applyParameters: internal.applyParameters,
     noteOn(noteNumber: number, _time: number) {
       const time = Math.max(_time, audioContext.currentTime);
-      const pr = bus.parameters;
-      const osc1Freq = calcOscFreq(
-        noteNumber,
-        pr.octave,
-        pr.osc1Coarse,
-        pr.osc1Fine,
-      );
-      const osc2Freq = calcOscFreq(
-        noteNumber,
-        pr.octave,
-        pr.osc2Coarse,
-        pr.osc2Fine,
-      );
-      osc1.type = "sawtooth";
-      osc2.type = "sawtooth";
-      osc1.frequency.value = osc1Freq;
-      osc2.frequency.value = osc2Freq;
-
-      osc1Gain.gain.value = pr.oscMix;
-      osc2Gain.gain.value = 1 - pr.oscMix;
-
-      ampGain.gain.cancelScheduledValues(time);
-      ampGain.gain.setValueAtTime(1, time);
-
-      const decayTime = 0.01 + power2(pr.ampDecay) * 2;
-      ampGain.gain.exponentialRampToValueAtTime(1e-3, time + decayTime);
+      state.noteNumber = noteNumber;
+      internal.applyParameters();
+      internal.triggerAttack(time);
     },
     noteOff(_time: number) {
       const time = Math.max(_time, audioContext.currentTime);
-      const pr = bus.parameters;
-      const releaseTime = 0.01 + power2(pr.ampRelease) * 2;
-      // ampGain.gain.cancelScheduledValues(time);
-      ampGain.gain.exponentialRampToValueAtTime(1e-3, time + releaseTime);
+      internal.triggerRelease(time);
+      state.noteNumber = -1;
     },
   };
 }
@@ -116,6 +139,7 @@ export function createSynthesizer(
   function affectParameters() {
     const pr = bus.parameters;
     outputNode.gain.value = power2(pr.outputVolume);
+    voice.applyParameters();
   }
   affectParameters();
 
