a04c1871e1f6b8a894cf55757fe69d20b2c24343
yahiro <yahiro07@users.noreply.github.com>
2026-07-25 15:29:43 +0900

    adjust key style

--- packages/sequencers/piano-roll-2/src/root/piano-roll-editor-view.tsx
+++ packages/sequencers/piano-roll-2/src/root/piano-roll-editor-view.tsx
@@ -101,22 +101,74 @@ const noteNameLabels = seqNumbers(configs.numKeys).map((i) => {
   return `${subNoteNames[subIndex]}${octave}`;
 });
 
+const keyTypes = [
+  "whiteL",
+  "black",
+  "whiteM",
+  "black",
+  "whiteH",
+  "whiteL",
+  "black",
+  "whiteM",
+  "black",
+  "whiteM",
+  "black",
+  "whiteH",
+];
+
+function getKeyStyle(subIndex: number, cellH: number) {
+  const keyType = keyTypes[subIndex];
+  const keyStyle: Record<string, any> = {
+    height: cellH,
+    left: 0,
+  };
+  if (keyType === "whiteL") {
+    keyStyle.height += cellH / 2;
+    keyStyle.bottom = 0;
+  } else if (keyType === "whiteM") {
+    keyStyle.height += cellH;
+    keyStyle.top = 0;
+    keyStyle.bottom = 0;
+    keyStyle.margin = "auto 0";
+  } else if (keyType === "whiteH") {
+    keyStyle.height += cellH / 2;
+    keyStyle.top = 0;
+  } else if (keyType === "black") {
+    keyStyle.zIndex = 1;
+    keyStyle.width = "63%";
+    keyStyle.background = "linear-gradient(to right, #222, #666)";
+    keyStyle.top = 0;
+    keyStyle.bottom = 0;
+    keyStyle.margin = "auto 0";
+  }
+  const withBottomBorder = subIndex === 0 || subIndex === 5;
+  if (withBottomBorder) {
+    keyStyle.borderBottom = `solid 0.5px #0003`;
+  }
+  if (subIndex === 0) {
+    keyStyle.background = "#e4e4e4";
+  }
+
+  return keyStyle;
+}
 const PitchLabelsColumn = () => {
   const { numKeys, cellH } = configs;
-
   return (
     <div>
       {seqNumbers(numKeys).map((i) => {
         const yi = numKeys - i - 1;
         const subIndex = yi % 12;
         const label = subIndex === 0 && noteNameLabels[i];
+        const keyStyle = getKeyStyle(subIndex, cellH);
         return (
-          <div class={cz(qu.wh(60, cellH).p(0.125).it)}>
+          <div class={cz(qu.wh(80, cellH).relative().it)}>
             <div
               class={cz(
-                qu.flexHA().h("full").it,
-                qu.bg("#fff").color("#444").fontSize(8).it,
+                qu.absolute().bg("#fff").w("full").it,
+                qu.flexHA().justify("end").p(1).it,
+                qu.color("#666").fontSize(11).cursor("pointer").it,
               )}
+              style={keyStyle}
             >
               {label}
             </div>
@@ -228,7 +280,7 @@ const Editor = () => {
   return (
     <div
       class={cz(
-        qu.flexH().gap(2).h(340).it,
+        qu.flexH().gap(0.5).h(340).it,
         qu.css({ overflowX: "hidden", overflowY: "scroll" }).it,
       )}
     >
