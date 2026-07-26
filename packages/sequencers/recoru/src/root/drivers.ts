1d89dcb88a1e0368ebaee402ac87ddb8e91d8d2b
yahiro <yahiro07@users.noreply.github.com>
2026-07-26 12:03:05 +0900

    calculate input note step position

--- packages/sequencers/recoru/src/root/drivers.ts
+++ packages/sequencers/recoru/src/root/drivers.ts
@@ -1,10 +1,15 @@
 import { queryUnitInterfaceForModule } from "wafer-host/unit-types";
-import { persistence } from "@/root/persistence";
 import { createSequencerEngine } from "@/root/sequencer";
 import { store } from "@/root/store";
 
 const unitInterface = queryUnitInterfaceForModule("wafer-v01", import.meta.url);
 const engine = createSequencerEngine(unitInterface);
+
+type ClockAnchor = {
+  timeFrom: number;
+  barFrom: number;
+  bpm: number;
+};
 
 export function setupUnit() {
   const st = store.state;
@@ -13,6 +18,22 @@ export function setupUnit() {
   engine.setLoopBars(st.loopBars);
   engine.setNotes(st.notes);
 
+  let clockAnchor: ClockAnchor | null = null;
+
+  const noteOutputPort = unitInterface?.createNoteOutputPort();
+
+  function getFloatStepPositionFromTime(time: number) {
+    const ac = unitInterface?.audioContext;
+    if (!ac || !clockAnchor) return undefined;
+    if (time <= 0) {
+      time = ac.currentTime;
+    }
+    const unitDuration = 60 / clockAnchor.bpm / 4;
+    return (
+      clockAnchor.barFrom * 16 + (time - clockAnchor.timeFrom) / unitDuration
+    );
+  }
+
   unitInterface?.completeSetup({
     unitAspects: {
       unitType: "sequencer",
@@ -20,27 +41,43 @@ export function setupUnit() {
     },
     clockHandlers: {
       start() {
+        clockAnchor = null;
         engine.start();
       },
       stop() {
         engine.stop();
         store.setPlayPos(null);
+        clockAnchor = null;
       },
-      processScheduling(_timeFrom, barFrom, _barTo, _bpm) {
+      processScheduling(timeFrom, barFrom, _barTo, bpm) {
         const stepPos = barFrom * 16;
         const playPosTotalSteps = Math.max(st.loopBars * 16, 32);
         const playPos = stepPos % playPosTotalSteps;
         store.setPlayPos(playPos);
-        const expectedPage = Math.floor(playPos / 32);
-        if (store.state.pageIndex !== expectedPage) {
-          store.setPageIndex(expectedPage);
-        }
+        clockAnchor = { timeFrom, barFrom, bpm };
       },
       processStep(stepIndex, time, unitDuration) {
         engine.processStep(stepIndex, time, unitDuration);
       },
     },
-    persistence: persistence,
+    noteInput: {
+      noteOn(noteNumber, time, velocity) {
+        console.log("noteOn", noteNumber, time, velocity);
+        const stepPos = getFloatStepPositionFromTime(time ?? 0);
+        console.log("stepPos", stepPos);
+        if (stepPos !== undefined) {
+          const loopSteps = store.state.loopBars * 16;
+          const si = Math.round(stepPos) % loopSteps;
+          console.log("si", si);
+        }
+
+        noteOutputPort?.noteOn(noteNumber);
+      },
+      noteOff(noteNumber) {
+        noteOutputPort?.noteOff(noteNumber);
+      },
+    },
+    // persistence: persistence,
   });
 }
 
