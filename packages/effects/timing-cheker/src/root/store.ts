9c81b013577e07cf58f1370b9be2a72488acc000
yahiro <yahiro07@users.noreply.github.com>
2026-07-30 16:35:31 +0900

    remove unused code

--- packages/effects/timing-cheker/src/root/store.ts
+++
@@ -1,8 +0,0 @@
-import { createStore } from "snap-store";
-import { LoopKey } from "@/root/definitions";
-
-export const store = createStore<{
-  selectedLoopKey: LoopKey | null;
-}>({
-  selectedLoopKey: null,
-});
