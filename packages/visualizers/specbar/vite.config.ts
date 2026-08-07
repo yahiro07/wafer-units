79b6dea09e589a6c5d647e6bf181c02ceab80179
yahiro <yahiro07@users.noreply.github.com>
2026-08-07 19:30:42 +0900

    change bundle file extension

--- packages/visualizers/specbar/vite.config.ts
+++ packages/visualizers/specbar/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "src/index.tsx"),
       formats: ["es"],
-      fileName: "index",
+      fileName: "index.sharable",
     },
     outDir: "../../../dist/specbar",
     emptyOutDir: true,
