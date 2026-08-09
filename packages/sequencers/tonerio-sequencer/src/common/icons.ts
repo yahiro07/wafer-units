46b2beb6bf53159cbed900e57a99fb1e0634313a
yahiro <yahiro07@users.noreply.github.com>
2026-08-10 02:54:48 +0900

    replace icon

--- packages/sequencers/tonerio-sequencer/src/common/icons.ts
+++
@@ -1,8 +0,0 @@
-import { FaCaretLeft, FaCaretRight, FaRandom, FaTrash } from "@preact-icons/fa";
-
-export const Icons = {
-  CaretLeft: FaCaretLeft,
-  CaretRight: FaCaretRight,
-  Random: FaRandom,
-  Trash: FaTrash,
-};
