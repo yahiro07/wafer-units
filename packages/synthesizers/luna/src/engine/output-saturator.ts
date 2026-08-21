0996064b3ff341489e5550a3499314d4b1e10218
yahiro <yahiro07@users.noreply.github.com>
2026-08-21 20:29:32 +0900

    adjust saturator curve

--- packages/synthesizers/luna/src/engine/output-saturator.ts
+++ packages/synthesizers/luna/src/engine/output-saturator.ts
@@ -1,29 +1,57 @@
-const INPUT_HEADROOM = 4;
+const INPUT_HEADROOM = 1.5;
 const CURVE_SIZE = 1024;
 
 function createTanhCurve(): Float32Array {
   const curve = new Float32Array(CURVE_SIZE);
   for (let i = 0; i < CURVE_SIZE; i += 1) {
     const u = (i / (CURVE_SIZE - 1)) * 2 - 1;
-    curve[i] = Math.tanh(u * INPUT_HEADROOM);
+    const x = u * INPUT_HEADROOM;
+    if (Math.abs(x) <= 1.5) {
+      curve[i] = x - (x * x * x) / 6.66;
+    } else {
+      curve[i] = Math.sign(x);
+    }
   }
   return curve;
 }
 
 export function createOutputSaturator(audioContext: AudioContext) {
   const inputNode = audioContext.createGain();
+  const dryGain = audioContext.createGain();
+  const preGain = audioContext.createGain();
   const shaperNode = audioContext.createWaveShaper();
-  inputNode.gain.value = (1 / INPUT_HEADROOM) * (0.5 / 0.46);
+  const wetGain = audioContext.createGain();
+  const outputNode = audioContext.createGain();
+
+  inputNode.gain.value = 1;
+  dryGain.gain.value = 1;
+  preGain.gain.value = 1 / INPUT_HEADROOM;
   shaperNode.oversample = "2x";
   shaperNode.curve = createTanhCurve();
-  inputNode.connect(shaperNode);
+  wetGain.gain.value = 0;
+  outputNode.gain.value = 1;
+
+  inputNode.connect(dryGain);
+  dryGain.connect(outputNode);
+  inputNode.connect(preGain);
+  preGain.connect(shaperNode);
+  shaperNode.connect(wetGain);
+  wetGain.connect(outputNode);
 
   return {
     inputNode,
-    outputNode: shaperNode,
+    outputNode,
+    setEnabled(enabled: boolean, time: number) {
+      dryGain.gain.setValueAtTime(enabled ? 0 : 1, time);
+      wetGain.gain.setValueAtTime(enabled ? 1 : 0, time);
+    },
     cleanup() {
       inputNode.disconnect();
+      dryGain.disconnect();
+      preGain.disconnect();
       shaperNode.disconnect();
+      wetGain.disconnect();
+      outputNode.disconnect();
     },
   };
 }
