ce5d55fdde4ebf2dacc567d39bf1a4fc7a3a494b
yahiro <yahiro07@users.noreply.github.com>
2026-08-26 08:39:52 +0900

    add pd saw

--- packages/synthesizers/luna-xt/src/engine/custom-waveforms.ts
+++ packages/synthesizers/luna-xt/src/engine/custom-waveforms.ts
@@ -1,8 +1,6 @@
 import { OscWave } from "@/defs/definitions";
 import { tunableSigmoid } from "@/utils/synth-math-utils";
 
-const periodicWaveCache: Partial<Record<OscWave, PeriodicWave | null>> = {};
-
 function getPulseWave(context: AudioContext, duty: number) {
   const terms = 32;
   const real = new Float32Array(terms);
@@ -18,17 +16,13 @@ function getPulseWave(context: AudioContext, duty: number) {
   });
 }
 
-function wrapCreatePeriodicWave(
-  context: AudioContext,
-  fn: (pp: number) => number,
-) {
+function makePeriodicWave(context: AudioContext, fn: (pp: number) => number) {
   const n = 256;
   const terms = 32;
   const ys = new Float32Array(n);
   for (let i = 0; i < n; i++) {
     const pp = i / n;
-    const y = 2 * pp - 1;
-    ys[i] = fn(y);
+    ys[i] = fn(pp);
   }
   const real = new Float32Array(terms);
   const imag = new Float32Array(terms);
@@ -48,24 +42,44 @@ function wrapCreatePeriodicWave(
   });
 }
 
-function getSaturatedSawWave(context: AudioContext, k: number) {
-  return wrapCreatePeriodicWave(context, (y) => tunableSigmoid(y, k));
-}
+const builders = {
+  saturatedSaw(ctx: AudioContext, k: number) {
+    return makePeriodicWave(ctx, (pp) => {
+      const y = 2 * pp - 1;
+      return tunableSigmoid(y, k);
+    });
+  },
+  pdSaw(ctx: AudioContext, level: number) {
+    return makePeriodicWave(ctx, (pp) => {
+      return Math.sin(2 * Math.PI * Math.pow(pp, 1 - level));
+    });
+  },
+};
 
-export function getCustomWaveform(
-  context: AudioContext,
+function getCustomWaveformCore(
+  ctx: AudioContext,
   wave: OscWave,
 ): OscillatorType | PeriodicWave {
   if (wave === OscWave.sawtooth) {
     return "sawtooth";
   } else if (wave === OscWave.sawtoothR) {
-    return (periodicWaveCache[wave] ??= getSaturatedSawWave(context, -0.6));
+    return builders.saturatedSaw(ctx, -0.6);
   } else if (wave === OscWave.pulse125) {
-    return (periodicWaveCache[wave] ??= getPulseWave(context, 0.125));
+    return getPulseWave(ctx, 0.125);
   } else if (wave === OscWave.pulse25) {
-    return (periodicWaveCache[wave] ??= getPulseWave(context, 0.25));
+    return getPulseWave(ctx, 0.25);
   } else if (wave === OscWave.ex1) {
-    return "square";
+    return builders.pdSaw(ctx, 0.98);
   }
   return "sine"; //fallback, not used
+}
+
+const waveformsCache: Partial<
+  Record<OscWave, PeriodicWave | OscillatorType | null>
+> = {};
+export function getCustomWaveform(
+  ctx: AudioContext,
+  wave: OscWave,
+): OscillatorType | PeriodicWave {
+  return (waveformsCache[wave] ??= getCustomWaveformCore(ctx, wave));
 }
