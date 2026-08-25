84b45865f13d6b47d12dae70156f7b3b1e2be01e
yahiro <yahiro07@users.noreply.github.com>
2026-08-25 16:34:46 +0900

    show notes timing

--- packages/effects/timing-cheker/src/root/store.ts
+++ packages/effects/timing-cheker/src/root/store.ts
@@ -5,9 +5,11 @@ export const store = createStore<{
   hostBpm: number;
   schedulingPlotterCanvas: HTMLCanvasElement | null;
   wavePlotterCanvasCh1: HTMLCanvasElement | null;
+  notesPlotterCanvas: HTMLCanvasElement | null;
 }>({
   barLength: 1,
   hostBpm: 0,
   schedulingPlotterCanvas: null,
   wavePlotterCanvasCh1: null,
+  notesPlotterCanvas: null,
 });
