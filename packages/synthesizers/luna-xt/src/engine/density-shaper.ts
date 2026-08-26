504d126c9bd8b4c09040e3d214ebc50df9ecf164
yahiro <yahiro07@users.noreply.github.com>
2026-08-27 04:09:46 +0900

    cache shaper buffer

--- packages/synthesizers/luna-xt/src/engine/density-shaper.ts
+++ packages/synthesizers/luna-xt/src/engine/density-shaper.ts
@@ -5,7 +5,6 @@ const INPUT_HEADROOM = 4;
 
 function fillDensityShaperCurveBuffer(
   curveBuffer: Float32Array,
-  _wave: number,
   level: number,
 ) {
   const sz = curveBuffer.length;
@@ -46,15 +45,23 @@ export function createDensityShaper(audioContext: AudioContext) {
     inputNode.connect(outputNode);
   }
 
+  let lastSetCurve: Float32Array | null = null;
+
   return {
     inputNode,
     outputNode,
     update(level: number) {
       const enabled = level > 0;
       if (enabled) {
-        const curve = densityShaperCurveBufferCache.update(-1, level);
-        if (shaperNode.curve !== curve) {
+        const steppedLevelKey = Math.floor(level * 10);
+        const steppedLevel = steppedLevelKey / 10;
+        const curve = densityShaperCurveBufferCache.getCached(
+          steppedLevelKey,
+          steppedLevel,
+        );
+        if (curve !== lastSetCurve) {
           shaperNode.curve = curve;
+          lastSetCurve = curve;
         }
         const scaler = mapUnaryTo(level, 0.1, 1);
         inputNode.gain.value = (1 / INPUT_HEADROOM) * scaler;
--- packages/synthesizers/luna-xt/src/engine/shaper-curve-buffer-cache.ts
+++ packages/synthesizers/luna-xt/src/engine/shaper-curve-buffer-cache.ts
@@ -1,20 +1,18 @@
-import { seqNumbers } from "@/utils/helpers";
-
 export function createShaperCurveBufferCache(
   size: number,
-  updateFn: (buffer: Float32Array, wave: number, level: number) => void,
+  updateFn: (buffer: Float32Array, level: number) => void,
 ) {
-  let currentKey = "";
-  const buffers = seqNumbers(2).map(() => new Float32Array(size));
+  const buffersCache: Record<number, Float32Array<ArrayBuffer>> = {};
+
+  function createBufferForLevel(level: number) {
+    const buffer = new Float32Array(size);
+    updateFn(buffer, level);
+    return buffer;
+  }
+
   return {
-    update(wave: number, level: number) {
-      const key = `${wave}_${level}`;
-      if (key !== currentKey) {
-        [buffers[0], buffers[1]] = [buffers[1], buffers[0]];
-        updateFn(buffers[0], wave, level);
-        currentKey = key;
-      }
-      return buffers[0];
+    getCached(key: number, level: number): Float32Array<ArrayBuffer> {
+      return (buffersCache[key] ??= createBufferForLevel(level));
     },
   };
 }
