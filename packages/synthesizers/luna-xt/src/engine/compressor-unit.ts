bc4acdfa9c6ee0a432b7bae8fbb5cd38a6dd4eda
yahiro <yahiro07@users.noreply.github.com>
2026-08-27 03:07:18 +0900

    add scaler

--- packages/synthesizers/luna-xt/src/engine/compressor-unit.ts
+++ packages/synthesizers/luna-xt/src/engine/compressor-unit.ts
@@ -2,7 +2,9 @@ import { invPower2, power2 } from "@/utils/synth-math-utils";
 
 export function createCompressorUnit(ac: AudioContext) {
   const inputNode = ac.createGain();
+  const preGain = ac.createGain();
   const compressor = ac.createDynamicsCompressor();
+  const makeupGain = ac.createGain();
   const outputNode = ac.createGain();
 
   inputNode.connect(outputNode);
@@ -14,14 +16,20 @@ export function createCompressorUnit(ac: AudioContext) {
     if (nextWireMode === wireMode) return;
 
     inputNode.disconnect();
+    preGain.disconnect();
     compressor.disconnect();
+    makeupGain.disconnect();
 
     if (nextWireMode === "active") {
-      inputNode.connect(compressor).connect(outputNode);
+      inputNode
+        .connect(preGain)
+        .connect(compressor)
+        .connect(makeupGain)
+        .connect(outputNode);
     } else if (nextWireMode === "bypass") {
       inputNode.connect(outputNode);
     }
-    nextWireMode = wireMode;
+    wireMode = nextWireMode;
   }
 
   return {
@@ -29,12 +37,25 @@ export function createCompressorUnit(ac: AudioContext) {
     outputNode,
     update(level: number) {
       updateConnection(level > 0 ? "active" : "bypass");
-      compressor.threshold.value = invPower2(level) * -80;
-      compressor.ratio.value = 1 + power2(level) * 18;
-      compressor.knee.value = 4;
-      compressor.attack.value = 0.005;
-      compressor.release.value = 0.04;
-      outputNode.gain.value = 1 + power2(level) * 5;
+      if (0) {
+        preGain.gain.value = 1;
+        compressor.threshold.value = invPower2(level) * -80;
+        compressor.ratio.value = 1 + power2(level) * 18;
+        compressor.knee.value = 4;
+        compressor.attack.value = 0.005;
+        compressor.release.value = 0.04;
+        makeupGain.gain.value = 1 + power2(level) * 5;
+      } else {
+        const scaler = 3;
+        preGain.gain.value = scaler;
+        compressor.threshold.value = invPower2(level) * -40;
+        compressor.ratio.value = 1 + power2(level) * 5;
+        compressor.knee.value = 8;
+        compressor.attack.value = 0.02;
+        compressor.release.value = 0.2;
+        const makeupGainValue = 1 + power2(level) * 2;
+        makeupGain.gain.value = makeupGainValue / scaler;
+      }
     },
     cleanup() {
       updateConnection(null);
