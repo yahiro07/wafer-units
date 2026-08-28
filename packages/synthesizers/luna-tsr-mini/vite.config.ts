dcfe2dce269ff188253f9d937d4cec646d4b2329
yahiro <yahiro07@users.noreply.github.com>
2026-08-29 02:15:33 +0900

    change project name

--- packages/synthesizers/luna-tsr-mini/vite.config.ts
+++ packages/synthesizers/luna-tsr-mini/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [preact(), UnoCSS()],
   resolve: { tsconfigPaths: true },
   build: {
-    outDir: "../../../dist/luna-xt",
+    outDir: "../../../dist/luna-tsr-mini",
     emptyOutDir: true,
   },
   server: { port: 3000 },
