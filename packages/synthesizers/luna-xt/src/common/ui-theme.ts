04ab91c54f32c1a9ad0226c78d27a446b532b4d6
yahiro <yahiro07@users.noreply.github.com>
2026-08-29 03:08:25 +0900

    create project

---
+++ packages/synthesizers/luna-xt/src/common/ui-theme.ts
@@ -0,0 +1,12 @@
+export const uiColors = {
+  clPageBg: "#eee",
+  clPageText: "#457",
+  clControlBg: "#ccc",
+  clControlEdge: "#0f0",
+  clAccent: "#f80",
+  clPlayPos: "#fc0",
+  clPrimary: "#8be",
+  clButtonBg: "#ccc",
+  clButtonText: "#667",
+  clKnobTick: "#08f",
+};
