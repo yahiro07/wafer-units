09f5eedae968750a81b4fd3c045aca83c2ee8531
yahiro <yahiro07@users.noreply.github.com>
2026-09-02 12:26:01 +0900

    change folder name

--- packages/synthesizers/altair-xm/vite.config.ts
+++ packages/synthesizers/altair-xm/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [preact(), UnoCSS()],
   resolve: { tsconfigPaths: true },
   build: {
-    outDir: "../../../dist/altair",
+    outDir: "../../../dist/altair-xm",
     emptyOutDir: true,
   },
   server: { port: 3000 },
