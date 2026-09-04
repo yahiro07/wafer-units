e6e213e68b8e6500cb465d5f028349b483e78069
yahiro <yahiro07@users.noreply.github.com>
2026-09-04 15:28:02 +0900

    implement engine

--- packages/effects/mxr2/src/core/definitions.ts
+++ packages/effects/mxr2/src/core/definitions.ts
@@ -1,19 +1,21 @@
 export type EffectParameters = {
+  inputGain: number;
   compThreshold: number;
-  compRatio: number;
-  compKnee: number;
   compAttack: number;
   compRelease: number;
-  makeupGain: number;
+  drive: number;
+  limit: number;
+  outputGain: number;
 };
 
 export const defaultEffectParameters: EffectParameters = {
+  inputGain: 0.5,
   compThreshold: 1,
-  compRatio: 0,
-  compKnee: 0,
   compAttack: 0,
   compRelease: 0,
-  makeupGain: 0.5,
+  drive: 0,
+  limit: 0,
+  outputGain: 0.5,
 };
 
 export type EffectEngine = {
