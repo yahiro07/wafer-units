ba110d03a63e84f65e2d66408e3ff365b243041b
yahiro <yahiro07@users.noreply.github.com>
2026-09-25 01:13:25 +0900

    create reverb

---
+++ packages/effects/toner-reverb/src/common/ui-theme.ts
@@ -0,0 +1,4 @@
+export const uiColors = {
+  clPanelBg: "#bbb",
+  clPanelText: "#444",
+};
---
+++ packages/effects/toner-reverb/src/core/definitions.ts
@@ -0,0 +1,11 @@
+export type EffectParameters = {
+  reverbTime: number;
+  reverbTone: number;
+  reverbMix: number;
+};
+
+export const defaultEffectParameters: EffectParameters = {
+  reverbTime: 0.5,
+  reverbTone: 0.5,
+  reverbMix: 0.5,
+};
